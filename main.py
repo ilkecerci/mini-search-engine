747c68b2325ea7c7d74b08f0e3110e291ac82d32
İlke Çerçi <ilkecerci18@gmail.com>
2026-05-04 21:33:56 +0300

    preprocessing test with first 500 words is successful

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from preprocessing import preprocess_text
 
+# testing to read doc01.txt 
+try:
+    with open("documents/doc01.txt", "r", encoding="utf-8") as file:
+        raw_text = file.read()
+        
+    # since it's too long, lets test the first 500 char only
+    sample_content = raw_text[:500]
+    clean_content = preprocess_text(sample_content)
+    
+    print("--- Original (First 500 chars) ---")
+    print(sample_content)
+    print("\n--- Processed Tokens ---")
+    print(clean_content)
+
+except FileNotFoundError:
+    print("Hata: 1.txt dosyası documents klasöründe bulunamadı!")
--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,27 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
+def preprocess_text(text):
+    # 1. Case folding
+    text = text.lower()
+    
+    # 2. Punctuation and symbol removal
+    text = re.sub(r'[^a-z\s]', '', text)
+    
+    # 3. Tokenization
+    tokens = text.split()
+    
+    # 4. Stop word removal
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+    
+    # 5. Stemming
+    stemmer = PorterStemmer()
+    final_terms = [stemmer.stem(word) for word in filtered_tokens]
+    
+    return final_terms
